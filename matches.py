1832f3317c9f91a29e549861745f2769e4129161
TageZ <tagezerby@gmail.com>
2023-08-29 11:49:34 -0500

    matches.py and updated teams
    Added to the new matches.py file a basic match object and the start of a function for getting fixtures and results. Also added the PL website to each team's json for web scrapping.

--- matches.py
+++ matches.py
@@ -1,4 +1,14 @@
+class Match:
+    def __init__(self, date, time, hometeam, awayteam):
+        self.date = date
+        self.time = time
+        self.hometeam = hometeam
+        self.awayteam = awayteam
 
+def getResults(team, num):
+    matches = 'N/A'
+    return matches
 
-class MatchesCog():
-    
+def getFixtures(team, num):
+    matches = "N/A"
+    return matches
