1312a51c44034b2c2de3fcdc49fe35e7522d07d8
TageZ <tagezerby@gmail.com>
2023-09-14 00:43:03 -0500

    Added to parseFixtures and parseResults basic functionality for getting the html code needed. Currently, that's mostly empty because the section isn't loaded in before the request, need a fix for this.

--- matches.py
+++ matches.py
@@ -1,10 +1,12 @@
 import json
+import requests
+import requests.auth
+from bs4 import BeautifulSoup
 
 with open('teams.json', 'r') as file:
     teams_data = json.load(file)
-    print(teams_data)
 
-teams = {team["name"]: team for team in teams_data}
+teams = {team['name']: team for team in teams_data}
 
 
 class Match:
@@ -14,15 +16,28 @@ class Match:
         self.hometeam = hometeam
         self.awayteam = awayteam
 
-# def parseFixtures(team):
-#     website =
 
-# def getFixtures(fixtures, team, num):
-#     matches = "N/A"
-#     return matches
+def parseFixtures(team):
+    website = teams[team]['website'] + '/fixtures'
+    teamWebsite = requests.get(website, timeout=15)
+    html = teamWebsite.text
+    return html
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find('section', {'class', 'fixtures'})
+    matches = table.findAll('div', attrs={'class': 'fixtures__date-container'})
+    return matchesW
 
-# def parseResults(team):
 
-# def getResults(results, team, num):
-#     matches = 'N/A'
-#     return matches
+def parseResults(team):
+    website = teams[team]['website'] + '/results'
+    teamWebsite = requests.get(website, timeout=15)
+    html = teamWebsite.text
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find('section', {'class', 'fixtures'}).text
+    matches = table.findAll('div', attrs={'class': 'fixtures__date-container'})
+    return matches
+
+
+def getResults(results, team, num):
+    matches = 'N/A'
+    return matches
