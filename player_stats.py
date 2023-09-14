0c8a0bb5a96272c5b3a95a620d5c3b612c16db84
TageZ <tagezerby@gmail.com>
2023-09-14 17:20:33 -0500

    Created basic functionality for retrieving a given team's top scorers/assiters.

--- player_stats.py
+++ player_stats.py
@@ -1,1 +1,59 @@
 # File for retrieving player statistics (top scorers/assisters).
+
+from bs4 import BeautifulSoup
+import requests
+import requests.auth
+
+
+def scrapeScorers(team):
+    if (not team):
+        team = 'premier-league'
+    website = 'https://www.bbc.com/sport/football/teams/' + team + '/top-scorers'
+    goalsWebsite = requests.get(website, timeout=10)
+    html = goalsWebsite.text
+    soup = BeautifulSoup(html, 'lxml')
+    goalScorers = soup.find('tbody', {'class', 'gel-long-primer'})
+    return goalScorers
+
+
+def scrapeAssisters(team):
+    if (not team):
+        team = 'premier-league'
+    website = 'https://www.bbc.com/sport/football/teams/' + team + '/top-scorers/assists'
+    assitsWebsite = requests.get(website, timeout=10)
+    html = assitsWebsite.text
+    soup = BeautifulSoup(html, 'lxml')
+    assisters = soup.find('tbody', {'class', 'gel-long-primer'})
+    return assisters
+
+
+def getScorers(goalScorers):
+    players = []
+    stats = goalScorers.findAll('tr')
+    for i in range(0, len(stats)):
+
+        player = stats[i].find(
+            'td', {'class', 'gs-o-table__cell gs-o-table__cell--left'}).find('span').find(
+            'span').text
+
+        goals = stats[i].find(
+            'td', {'class', 'gs-o-table__cell gs-o-table__cell--right'}).text
+
+        players.append({'Name': player, 'Goals': goals})
+    return players
+
+
+def getAssisters(assisters):
+    players = []
+    stats = assisters.findAll('tr')
+    for i in range(0, len(stats)):
+
+        player = stats[i].find(
+            'td', {'class', 'gs-o-table__cell gs-o-table__cell--left'}).find('span').find(
+            'span').text
+
+        assists = stats[i].find(
+            'td', {'class', 'gs-o-table__cell gs-o-table__cell--right'}).text
+
+        players.append({'Name': player, 'Assists': assists})
+    return players
