b60980036d4d30d6da8fcbba218f105e80b253ed
TageZ <tagezerby@gmail.com>
2023-09-18 12:58:18 -0500

    Updated Front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
 
   const [data, setData] = useState([{}])
 
   useEffect(() => {
+
     fetch('/teams').then(
       res => res.json()
     ).then(
@@ -13,28 +14,46 @@ function App() {
         console.log(data)
       }
     )
+
   }, [])
 
   return (
-    
-    <div className="logos">
-    <h1>Team Logos</h1>
-      {(typeof data.name === 'undefined') ? (
-        <p>Loading...</p>
-      ) : (
-        data.name.map((name, i) => (
-          <div className="logo" key={i}>
-            <img 
-              className="hover-zoom" 
-              src={name} 
-              alt="club logo" 
-              width='200' 
-              height='200'>
-            </img>
-          </div>
-        ))
-      )}
-    </div>
+    <div className='sections'>
+      <div className='banner'>
+        <span className='app-title'>
+          Premier League Resource
+        </span>
+        <div className='pl-logo-container'>
+          <img
+            className="pl-logo"
+            src={process.env.PUBLIC_URL + '/premier-league.svg'}
+            alt="Premier League Logo"
+            width='600'
+            height='600'
+          >
+          </img>
+        </div>
+      </div>
+
+
+      <div className="logos">
+        {(typeof data.name === 'undefined') ? (
+          <p>Loading...</p>
+        ) : (
+          data.name.map((name, i) => (
+            <div className="logo" key={i}>
+              <img
+                className="hover-zoom"
+                src={name}
+                alt="club logo"
+                width='80'
+                height='80'>
+              </img>
+            </div>
+          ))
+        )}
+      </div>
+    </div >
   )
 }
 
--- python_backend/main.py
+++ python_backend/main.py
@@ -4,6 +4,7 @@ import json
 with open('teams.json', 'r') as file:
     teams_data = json.load(file)
 
+teams_data = sorted(teams_data, key=lambda x: x['name'])
 names = [item["logo"] for item in teams_data]
 team_names = {'name': names}
 
